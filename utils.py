e8ffe7a9cfd63ffbb19ffde4b9f87cddc04ef953
Nikesh <nikeshpenala15@gmail.com>
2025-07-19 06:31:05 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def eye_aspect_ratio(eye):
+    # Compute the euclidean distances between the two sets of vertical eye landmarks (x, y)
+    A = np.linalg.norm(eye[1] - eye[5])
+    B = np.linalg.norm(eye[2] - eye[4])
+
+    # Compute the euclidean distance between the horizontal eye landmark (x, y)
+    C = np.linalg.norm(eye[0] - eye[3])
+
+    # Compute the eye aspect ratio
+    ear = (A + B) / (2.0 * C)
+    return ear
